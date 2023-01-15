ec012b45eef597370b90c4f5d76646887a5c7969
MayankCodr <96002289+MayankCodr@users.noreply.github.com>
2023-01-15 10:54:45 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,18 @@
+function preload(){
+
+}
+function setup(){
+    canvas = createCanvas(550, 500);
+    canvas.position(550, 160);
+    video = createCapture(VIDEO);
+    video.size(550, 500);
+}
+function draw(){
+    background('grey');
+}
+function modelLoaded(){
+
+}
+function gotPoses(){
+    
+}
